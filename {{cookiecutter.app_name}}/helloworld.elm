49e8c884f77033c0ebc2a3f29b4672e3dc10c36e
CkuT <cku@gmx.com>
2017-04-10 10:58:14 +0200

    Update for Elm 0.18

--- {{cookiecutter.app_name}}/helloworld.elm
+++ {{cookiecutter.app_name}}/helloworld.elm
@@ -1,16 +1,16 @@
+module Main exposing (..)
+
 import Html exposing (Html, Attribute, div, input, text)
-import Html.App as Html
 import Html.Attributes exposing (..)
 import Html.Events exposing (onInput)
 
 
-
 main =
-  Html.beginnerProgram
-    { model = model
-    , view = view
-    , update = update
-    }
+    Html.beginnerProgram
+        { model = model
+        , view = view
+        , update = update
+        }
 
 
 
@@ -18,13 +18,13 @@ main =
 
 
 type alias Model =
-  { content : String
-  }
+    { content : String
+    }
 
 
 model : Model
 model =
-  Model ""
+    Model ""
 
 
 
@@ -32,13 +32,14 @@ model =
 
 
 type Msg
-  = Change String
+    = Change String
+
 
 update : Msg -> Model -> Model
 update msg model =
-  case msg of
-    Change newContent ->
-      { model | content = newContent }
+    case msg of
+        Change newContent ->
+            { model | content = newContent }
 
 
 
@@ -47,7 +48,7 @@ update msg model =
 
 view : Model -> Html Msg
 view model =
-  div []
-    [ input [ placeholder "Text to input", onInput Change ] []
-    , div [] [ text (toString model.content) ]
-    ]
+    div []
+        [ input [ placeholder "Text to input", onInput Change ] []
+        , div [] [ text (toString model.content) ]
+        ]
